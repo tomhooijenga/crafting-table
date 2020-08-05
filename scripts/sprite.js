08d3cb36e05425b1c9c3d7b41cd304f5831c73e7
Tom Hooijenga <tomhooijenga@gmail.com>
2020-08-05 21:58:56 +0200

    Cleanup sprite generation, add more animated icons

--- scripts/sprite.js
+++ scripts/sprite.js
@@ -3,67 +3,20 @@ const fs = require('fs');
 
 // https://minecraft.gamepedia.com/Template:InvSprite#Example
 // https://minecraft.gamepedia.com/Module:InvSprite
-
 // https://minecraft.gamepedia.com/Category:Animated_inventory_icons
 
-
-let sprite = fs.readFileSync('./items.txt', 'utf-8');
-
-const itemsByDisplayName = data.itemsArray.reduce((obj, item) => {
-  obj[item.displayName] = item;
-  return obj;
-}, {})
-
-const itemsByName = data.itemsByName;
-let notFound = 0;
-
-function makeSprite(sprite) {
-  return sprite
-    .trim()
-    .split('\n')
-    .map((line) => {
-      const [, displayName, pos] = line.match(/(?:\[['"])?(.+?)(?:["']\])? = { pos = (\d+)/);
-      const name = displayName
-        .toLowerCase()
-        .replace(/ /g, '_')
-        .replace(/[^a-z\d_]/g, '')
-        .replace(/_?revision_1/, '');
-
-      const x = (pos - 1) * 32 % 1024;
-      const y = Math.floor((pos - 1) / 32) * 32;
-      return {
-        displayName,
-        name,
-        pos: +pos,
-        x,
-        y
-      };
-    })
-    .reduce((obj, item) => {
-      let id;
-
-      if (!itemsByDisplayName[item.displayName] && !itemsByName[item.name]) {
-        return obj;
-      } else {
-        id = (itemsByDisplayName[item.displayName] || itemsByName[item.name]).id
-      }
-
-      obj[id] = {
-        id,
-        animated: false,
-        ...item
-      };
-      return obj;
-    }, {})
+const aliases = {
+  'flower_banner_pattern': 'banner_pattern',
+  'creeper_banner_pattern': 'banner_pattern',
+  'skull_banner_pattern': 'banner_pattern',
+  'mojang_banner_pattern': 'banner_pattern',
+  'globe_banner_pattern': 'banner_pattern',
+  'piglin_banner_pattern': 'banner_pattern',
+  'lingering_potion': 'lingering_water_bottle',
+  'splash_potion': 'splash_water_bottle',
 }
 
-const spriteData = Object.fromEntries(
-  Object
-    .values(makeSprite(sprite))
-    .map(({x, y, id}) => [id, {x, y, id}])
-);
-
-[
+const animated = new Set([
   'prismarine',
   'prismarine_stairs',
   'prismarine_slab',
@@ -75,15 +28,88 @@ const spriteData = Object.fromEntries(
   'compass',
   'experience_bottle', // Bottle o' enchanting
   'potion',
-].forEach((name) => {
-  const item = itemsByName[name];
-  spriteData[item.id] = {
-    id: item.id,
-    animated: true,
-    name,
-  }
-});
+  'enchanted_book',
+  'end_crystal',
+  'enchanted_golden_apple',
+  'debug_stick',
+  'lodestone',
+  'nether_star',
+  'stonecutter',
+  'warped_hyphae',
+  'warped_stem',
+]);
 
-fs.writeFileSync('../src/assets/data/sprite.json', JSON.stringify(spriteData, null, 2));
+function parseSprite(spriteText) {
+  return spriteText
+    .trim()
+    .split('\n')
+    .map((line) => {
+      const [, displayName, position] = line.match(/(?:\[['"])?(.+?)(?:["']\])? = { pos = (\d+)/);
 
-module.exports = makeSprite.bind(null, sprite);
+      return {
+        displayName,
+        position: parseInt(position),
+        animated: false,
+      }
+    })
+    .map((icon) => {
+      // Try to construct an item name from the display name. Does not always work, for example
+      // bottle o' enchanting => experience_bottle.
+      const name = icon.displayName
+        .toLowerCase()
+        .replace(/ /g, '_')
+        .replace(/[^a-z\d_]/g, '')
+        .replace(/_?revision_1/, '');
+
+      return {
+        name,
+        ...icon,
+      }
+    })
+    .map((icon) => {
+      const position = icon.position;
+
+      // Sprite data is 1-indexed. Sprite is 1024 wide.
+      const x = (position - 1) * 32 % 1024;
+      const y = Math.floor((position - 1) / 32) * 32;
+
+      return {
+        x,
+        y,
+        ...icon,
+      }
+    });
+}
+
+const sprite = parseSprite(fs.readFileSync('./items.txt', 'utf-8'));
+const icons = Object
+  .entries(data.itemsByName)
+  .map(([name, item]) => {
+    if (animated.has(name)) {
+      return {
+        id: item.id,
+        name,
+        animated: true,
+      }
+    }
+
+    const {x, y} = sprite.find((icon) => {
+      return icon.name === name
+        || icon.displayName === item.displayName
+        || icon.name === aliases[name];
+    });
+
+    return {
+      id: item.id,
+      name,
+      animated: false,
+      x,
+      y
+    }
+  })
+  .reduce((obj, icon) => {
+    obj[icon.id] = icon;
+    return obj;
+  }, {});
+
+fs.writeFileSync('../src/assets/data/sprite.json', JSON.stringify(icons));
