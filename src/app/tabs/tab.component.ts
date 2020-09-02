f419aa7f8f7f1d6d5fb5c43db43c34cad5c4bec1
Tom Hooijenga <tomhooijenga@gmail.com>
2020-09-02 12:47:55 +0200

    Add min height to tabs

--- src/app/tabs/tab.component.ts
+++ src/app/tabs/tab.component.ts
@@ -1,10 +1,15 @@
-import {Component, Input} from '@angular/core';
+import {Component, HostBinding, Input} from '@angular/core';
 
 @Component({
   selector: 'app-tab',
   templateUrl: './tab.component.html',
 })
 export class TabComponent {
+  @HostBinding('class.active')
+  get contentActive(): boolean {
+    return this.active;
+  }
+
   @Input() tabTitle: string;
   @Input() active = false;
 }
