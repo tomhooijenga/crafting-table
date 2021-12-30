d66cfcf02a50e1bf6a05aa01471702515ddad4df
Tom <tomhooijenga@gmail.com>
2021-12-30 02:37:39 +0100

    Fix panel border

--- vue/src/components/Panel.vue
+++ vue/src/components/Panel.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="p-[6px] border-2 bg-[#C6C6C6] [border-color:#DBDBDB #5B5B5B #5B5B5B #DBDBDB]">
+  <section class="p-[6px] border-2 bg-[#C6C6C6] border-t-[#DBDBDB] border-r-[#5B5B5B] border-b-[#5B5B5B] border-l-[#DBDBDB]">
     <slot />
   </section>
 </template>
