6b245312c1abf3331f131d0030bd413cdd5df4ed
Tom <tomhooijenga@gmail.com>
2021-12-30 20:15:34 +0100

    Drop on outside click

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="inline-flex flex-col items-center">
+  <section ref="root" class="inline-flex flex-col items-center">
     <CraftingGrid />
     <Inventory class="h-64" />
   </section>
@@ -10,4 +10,15 @@
 import Inventory from '@/components/Inventory.vue'
 import HoverIcon from "./components/DragIcon.vue";
 import CraftingGrid from "./components/CraftingGrid.vue";
+import {useSelectionStore} from "./store";
+import {ref} from "vue";
+
+const selection = useSelectionStore();
+const root = ref();
+
+document.body.addEventListener('click', function clickOutsideEvent (event) {
+  if (!(root.value == event.target || root.value.contains(event.target))) {
+    selection.drop();
+  }
+})
 </script>
