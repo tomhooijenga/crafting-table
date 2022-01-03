00770522574e8463a9884964b7d78ff449dfc6d7
Tom <tomhooijenga@gmail.com>
2022-01-03 21:01:55 +0100

    Rounded border on panels

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -1,6 +1,6 @@
 <template>
   <section
-    class="p-2 border-2 bg-[#C6C6C6] border-t-[#DBDBDB] border-r-[#5B5B5B] border-b-[#5B5B5B] border-l-[#DBDBDB]"
+    class="p-2 border-2 bg-[#C6C6C6] border-t-[#DBDBDB] border-r-[#5B5B5B] border-b-[#5B5B5B] border-l-[#DBDBDB] rounded"
   >
     <slot />
   </section>
