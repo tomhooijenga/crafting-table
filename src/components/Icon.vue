72745c6fb902436e273d842d820423c8c0a3f79a
Tom <tomhooijenga@gmail.com>
2022-01-03 01:01:17 +0100

    Fix some icons dropping

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,28 +1,21 @@
 <template>
-  <img
-    v-if="icon.animated"
-    :title="item.displayName"
-    class="w-8 h-8"
-    :src="animated[`/src/assets/icons/${item.name}.gif`].default"
-  />
-  <div
-    v-else
-    :title="item.displayName"
-    class="relative w-8 h-8 bg-[url('@/assets/sprite.png')]"
-    :style="{ backgroundPosition: `-${icon.x}px -${icon.y}px` }"
+  <div :title="item.displayName"
+       class="relative w-8 h-8"
+       :style="style"
   >
     <span
-      v-if="amount && amount > 1"
-      v-text="amount"
-      class="text-white [text-shadow:1px_1px_0_black] text-xl leading-none absolute -right-0.5 -bottom-0.5"
+        v-if="amount && amount > 1"
+        v-text="amount"
+        class="text-white [text-shadow:1px_1px_0_black] text-xl leading-none absolute -right-0.5 -bottom-0.5"
     />
   </div>
 </template>
 
 <script setup lang="ts">
-import { Item, Icon } from "@/types";
-import { computed } from "vue";
-import { getIcon } from "@/lib/icon";
+import {Icon, Item} from "@/types";
+import {computed} from "vue";
+import {getIcon} from "@/lib/icon";
+import sprite from '@/assets/sprite.png';
 
 const props = defineProps<{
   item: Item;
@@ -34,4 +27,18 @@ const icon = computed((): Icon => {
 });
 
 const animated = import.meta.globEager("/src/assets/icons/*.gif");
+
+const style = computed(() => {
+  if (icon.value.animated) {
+    return {
+      backgroundImage: `url(${animated[`/src/assets/icons/${props.item.name}.gif`].default})`
+    }
+  }
+
+  return {
+    backgroundImage: `url(${sprite})`,
+    backgroundPosition: `-${icon.value.x}px -${icon.value.y}px`
+  }
+})
+
 </script>
