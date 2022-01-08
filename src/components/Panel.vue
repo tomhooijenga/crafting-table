be98696a4b6d2601e31c85a9892b93182cac4c37
Tom <tomhooijenga@gmail.com>
2022-01-08 15:55:44 +0100

    Fix colors

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -1,6 +1,6 @@
 <template>
   <section
-    class="p-2 border-2 bg-[#C6C6C6] border-t-[#DBDBDB] border-r-[#5B5B5B] border-b-[#5B5B5B] border-l-[#DBDBDB] rounded"
+    class="p-2 border-4 bg-[#C6C6C6] border-t-[#DBDBDB] border-r-[#555555] border-b-[#555555] border-l-[#DBDBDB] rounded"
   >
     <slot />
   </section>
