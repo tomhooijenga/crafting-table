8817306477db747e935c309b8b7f0ba9860dfcf9
Tom <tomhooijenga@gmail.com>
2022-01-19 22:45:21 +0100

    Fix tile background color

--- src/components/CraftingGrid.vue
+++ src/components/CraftingGrid.vue
@@ -4,41 +4,42 @@
       <span class="text-lg col-start-2">Crafting</span>
 
       <Sprite
-          id="crafting_book"
-          @click="uiStore.showBook = !uiStore.showBook"
-          class="my-auto mr-4"
+        id="crafting_book"
+        @click="uiStore.showBook = !uiStore.showBook"
+        class="my-auto mr-4"
       />
 
       <div class="flex">
         <div class="grid grid-cols-3">
           <GridTile
-              v-for="(n, index) in grid.length"
-              :item="grid[index].value.item"
-              :amount="grid[index].value.amount"
-              @click.exact="writableTileStore.click(grid[index])"
-              @click.right="writableTileStore.rightClick(grid[index])"
-              @click.shift.exact="writableTileStore.shiftClick(grid[index])"
-              @dblclick="writableTileStore.dblClick(grid[index])"
-              @mousedown="writableTileStore.mousedown(grid[index])"
-              @mouseup="writableTileStore.mouseup(grid[index])"
-              @mouseenter="writableTileStore.mouseenter(grid[index])"
-              @mouseleave="writableTileStore.mouseleave(grid[index])"
-              :class="{ 'bg-[#a17171]': !craftingGridStore.craftable }"
+            v-for="(n, index) in grid.length"
+            :item="grid[index].value.item"
+            :amount="grid[index].value.amount"
+            :craftable="craftingGridStore.craftable"
+            @click.exact="writableTileStore.click(grid[index])"
+            @click.right="writableTileStore.rightClick(grid[index])"
+            @click.shift.exact="writableTileStore.shiftClick(grid[index])"
+            @dblclick="writableTileStore.dblClick(grid[index])"
+            @mousedown="writableTileStore.mousedown(grid[index])"
+            @mouseup="writableTileStore.mouseup(grid[index])"
+            @mouseenter="writableTileStore.mouseenter(grid[index])"
+            @mouseleave="writableTileStore.mouseleave(grid[index])"
           />
         </div>
 
         <Sprite id="crafting_arrow" class="my-auto mx-[0.875rem]" />
 
         <GridTile
-            class="my-auto h-[3.375rem] w-[3.375rem]"
-            :item="craftingGridStore.item"
-            :amount="craftingGridStore.amount"
-            @mousedown="craftingGridStore.resetIfPreview()"
-            @click.exact="craft(false)"
-            @click.right.exact="craft(false)"
-            @click.shift.exact="craft(true)"
-            @click.right.shift.exact="craft(true)"
-            :class="{ 'bg-[#a17171]': !craftingGridStore.craftable }"
+          class="my-auto h-[3.375rem] w-[3.375rem]"
+          :item="craftingGridStore.item"
+          :amount="craftingGridStore.amount"
+          :craftable="craftingGridStore.craftable"
+          @mousedown="craftingGridStore.resetIfPreview()"
+          @click.exact="craft(false)"
+          @click.right.exact="craft(false)"
+          @click.shift.exact="craft(true)"
+          @click.right.shift.exact="craft(true)"
+          :class="{ 'bg-[#a17171]': !craftingGridStore.craftable }"
         />
       </div>
     </div>
--- src/components/GridTile.vue
+++ src/components/GridTile.vue
@@ -1,6 +1,7 @@
 <template>
   <div
-    class="flex justify-center items-center border-2 border-t-[#370501] border-r-white border-b-white border-l-[#370501] bg-[#8B8B8B]"
+    class="flex justify-center items-center border-2 border-t-[#370501] border-r-white border-b-white border-l-[#370501]"
+    :class="{ 'bg-[#8B8B8B]': craftable, 'bg-[#a17171]': !craftable }"
   >
     <div class="hover:bg-white/30">
       <Icon :item="item" :amount="amount" />
@@ -12,8 +13,11 @@
 import Icon from "@/components/Icon.vue";
 import { Item } from "@/types";
 
-defineProps<{
+withDefaults(defineProps<{
   item: Item;
   amount?: number;
-}>();
+  craftable?: boolean
+}>(), {
+  craftable: true
+});
 </script>
